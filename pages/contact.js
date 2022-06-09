3a4cfaaa40e491d77828a98be44ccadc9bbfb312
AMRESH <amreshaiio@gmail.com>
2022-06-09 12:35:43 -0700

    Static content only

---
+++ pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Contact = () => {
+  return (
+    <div>Contact
+    <nav>
+      <Link href='/' ><a><li>Home</li></a></Link>
+    </nav>
+    </div>
+  )
+}
+
+export default Contact
